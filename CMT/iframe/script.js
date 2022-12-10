63f6b48273b9736ea447ff42d851c85329f464f3
PviGetGames <26828922+PviGetGames@users.noreply.github.com>
2022-12-10 18:28:17 -0500

    create window reader

---
+++ CMT/iframe/script.js
@@ -0,0 +1,3 @@
+window.addEventListener("load", () => {
+    document.getElementById('yeet').innerHTML = `<iframe src="https://crazyalgebra.com/" height="${window.innerHeight;}" width="${window.innerWidth;}" frameborder="0"></iframe>`
+});
